57caa4bf11b8de25b1a6c1714ff021e281da0118
Roland Garzon <roljha@hotmail.com>
2017-03-25 19:36:49 -0500

    Servicio WEB Servicios PQR

---
+++ WCFServiceRecaudo/BaseRespuesta.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace WCFServiceRecaudo
+{
+    [DataContract]
+    public class BaseRespuesta
+    {
+        [DataMember]
+        public string MensajeRespuestaOk { get; set; }
+        [DataMember]
+        public string MensajeRespuestaError { get; set; }
+    }
+}
---
+++ WCFServiceRecaudo/IWSRecaudo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceRecaudo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IWSRecaudo
+    {
+
+        [OperationContract]
+        List<DatosSuscriptor> obtenerDatosSuscriptor(long nuIdsuscriptor);
+
+        [OperationContract]
+        List<DatosCuponPago> obtenerDatosCuponPago(long nuIdcuponpago);
+
+        [OperationContract]
+        Boolean obtenerEstadoFacturacion(long nuIdcuponpago);
+
+        [OperationContract]
+        List<DatosCuponPago> generarDatosCuponPago(long nuIdSuscriptor, int nuValorCuponPago, long nuIdCuentaCobro);
+
+    }
+
+
+}
